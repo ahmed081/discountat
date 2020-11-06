67eb441a0c07d2722eb817dab38f54560e2fc6c1
ahmed081 <assimi33.y@gmail.com>
2020-11-06 23:08:40 +0100

    third commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,13 +39,12 @@
 				<div class="container">
 					<div class="row text-center  mx-auto">
 						<div class="col-lg-8 col-sm-12 center-block align-items-center construction  ">
-							<div class="text-center mb-4">
-								<img src="{{asset('template/images/brand/logo.png')}}" class="header-brand-img desktop-lgo" alt="Clont logo">
-								<img src="{{asset('template/images/brand/logo1.png')}}" class="header-brand-img dark-logo" alt="Clont logo">
+							<div class="text-center">
+								<h1 class="display-5 mb-1"><strong>wait for it</strong></h1>
 							</div>
 							<div class="construction-body">
 								<div class="card-body ">
-									<h2 class="display-5 mb-2"><strong>Under Construction</strong></h2>
+									<h2 class="display-5 mb-3"><strong>Under Construction</strong></h2>
 									<h4>Our website is in Under construction</h4>
 									<div id="launch_date"></div>
 								</div>
